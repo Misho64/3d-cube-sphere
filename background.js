4d6a584008399b11b4613b48fe531de129cea8c5
Misho64 <61743234+Misho64@users.noreply.github.com>
2025-07-28 05:15:25 +0300

    Update background.js

--- background.js
+++ background.js
@@ -2,25 +2,21 @@ function setup() {
   let cnv = createCanvas(windowWidth, windowHeight, WEBGL);
   cnv.position(0, 0);
   cnv.style('z-index', '-1');
-  angleMode(DEGREES);
-  strokeWeight(2);
   noFill();
-  stroke(32, 8, 64);
+  stroke(255, 100);
+  strokeWeight(1);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background(255, 200, 220);
-  orbitControl();
-
-  for (let zAngle = 0; zAngle < 180; zAngle += 30) {
-    for (let xAngle = 0; xAngle < 360; xAngle += 30) {
-      push();
-      rotateZ(zAngle);
-      rotateX(xAngle);
-      translate(0, 300, 0);
-      box(20);
-      pop();
-    }
+  background(15, 15, 30);
+  rotateY(frameCount * 0.2);
+  for (let i = 0; i < 200; i++) {
+    push();
+    rotateX(sin(frameCount * 0.5 + i) * 100);
+    translate(200, 0, 0);
+    box(5);
+    pop();
   }
 }
 
