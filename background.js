e477f6a650aa86330965ce407a305b0eeecab4b7
Misho64 <61743234+Misho64@users.noreply.github.com>
2025-07-28 05:27:00 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,27 @@
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-  canvas.position(0, 0);
-  canvas.style('z-index', '-1');
-  canvas.style('position', 'fixed');
+  createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
-  strokeWeight(2);
+  strokeWeight(5);
   noFill();
-  stroke(160, 100, 255); // لون بنفسجي ناعم
+  stroke(32, 8, 64);
 }
 
 function draw() {
-  background(0, 0, 0, 0); // خلفية شفافة
+  background(250, 180, 200);
   orbitControl();
 
-  for (let z = 0; z < 180; z += 30) {
-    for (let x = 0; x < 360; x += 30) {
+  for (let zAngle = 0; zAngle < 180; zAngle += 30) {
+    for (let xAngle = 0; xAngle < 360; xAngle += 30) {
       push();
-      rotateZ(z);
-      rotateX(x);
-      translate(0, 300, 0);
-      box(10); // شكل المكعب
+      rotateZ(zAngle);
+      rotateX(xAngle);
+      translate(0, 400, 0);
+      box();
       pop();
     }
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
